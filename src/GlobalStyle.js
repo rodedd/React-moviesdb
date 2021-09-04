b25751903af108350087c2e062ebc322bad74f63
Eduardo <96.rodedd@gmail.com>
2021-09-04 18:20:37 -0500

    header

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,26 +1,27 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
   :root {
-    --maxWidth: 1280px;
+    --maxWidth: 1280px; 
     --white: #fff;
     --lightGrey: #eee;
-    --medGrey: #353535;
+    --medGrey: #353535; 
     --darkGrey: #1c1c1c;
     --fontSuperBig: 2.5rem;
     --fontBig: 1.5rem;
     --fontMed: 1.2rem;
     --fontSmall: 1rem;
-    }
+  }
 
   * {
     box-sizing: border-box;
-    font-family: 'Abel', sans-seriff;
+    font-family: 'Abel', sans-serif;  
   }
 
-  body {
+  body {  
     margin: 0;
     padding: 0;
+    box-sizing: border-box;
 
     h1 {
       font-size: 2rem;
